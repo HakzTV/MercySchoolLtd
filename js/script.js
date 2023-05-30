b3269a28f942717a3a89aa65f70667ded5d52189
Telvin Varfley <telvinvarfley@gmail.com>
2023-05-30 12:19:33 +0000

    Ui and Content update

--- js/script.js
+++ js/script.js
@@ -9,8 +9,25 @@ $(function() {
       AOS.refresh();
     });
   });
-
+  $(".number").each(function () {
+      $(this)
+        .prop("Counter", 0)
+        .animate(
+          {
+            Counter: $(this).text(),
+          },
+          {
+            duration: 4000,
+            easing:"swing",
+            step:function (now) {
   
+              now = Number(Math.ceil(now)).toLocaleString('en');
+                                    $(this).text(now);
+            },
+          }
+        );
+    });
+    
 function onElementHeightChange(elm, callback) {
     var lastHeight = elm.clientHeight
     var newHeight;
